6661e61065392905af6404dae419c12f94da133f
AidanD-C <dc.aidan@gmail.com>
2026-05-17 13:58:12 -0400

    Learned a bit about glm

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+    glfwWindowHint(GLFW_SAMPLES, 4);
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     glfwMakeContextCurrent(window);
     
@@ -41,6 +41,8 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    glEnable(GL_MULTISAMPLE);
 
     glViewport(0, 0, 800, 600);
 
